66d245b0a0c73135cb3c11e959f212c465c48d15
AbrarKhalil26 <abrarkhalil135@gmail.com>
2026-04-17 16:02:50 +0200

    first comment

---
+++ src/modules/auth/auth.controller.ts
@@ -0,0 +1,59 @@
+import { Router } from "express";
+import AuthService from "./auth.service";
+import * as AuthValidation from "./auth.validation";
+import { validation } from "../../common/middleware/validation";
+import { authentication } from "../../common/middleware/authentication";
+
+const authRouter = Router();
+// --------------------------------
+// Sign Up ------------------------
+authRouter.post(
+  "/signup",
+  validation(AuthValidation.signUpSchema),
+  AuthService.signUp,
+);
+
+// -----------------------------------
+// Confirm email ---------------------
+authRouter.post(
+  "/verify-email",
+  validation(AuthValidation.confirmEmailSchema),
+  AuthService.confirmEmail,
+);
+
+// -------------------------------
+// Login -------------------------
+authRouter.post(
+  "/login",
+  validation(AuthValidation.loginSchema),
+  AuthService.login,
+);
+
+// ------------------------------------
+// Forget Password --------------------
+authRouter.patch(
+  "/forget-password",
+  validation(AuthValidation.emailSchema),
+  AuthService.forgetPassword,
+);
+
+// -----------------------------------
+// Reset Password --------------------
+authRouter.patch(
+  "/reset-password",
+  validation(AuthValidation.resetPasswordSchema),
+  AuthService.resetPassword,
+
+);
+
+// -----------------------------------
+// Update Password --------------------
+authRouter.patch(
+  "/update-password",
+  authentication,
+  validation(AuthValidation.updatePasswordSchema),
+  AuthService.updatePassword,
+);
+
+
+export default authRouter;
